52d5850f1bc3428c38cdde11db04e28934c5b502
Steven Taglohner <taglohner@me,com>
2017-08-29 22:59:49 +0100

    - Created Xcode project

---
+++ WordPressBlogApp/WordPressBlogApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordPressBlogApp
+//
+//  Created by Steven Taglohner on 29/08/2017.
+//  Copyright © 2017 Steven Taglohner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
