546c704e4783188fe1574ea73caa223dcc561d67
Steven Taglohner <taglohner@me,com>
2017-08-31 00:53:58 +0100

    - Created the MainTableViewController, MainTableViewCell, a network support file.

---
+++ WordPressBlogApp/WordPressBlogApp/MainTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MainTableViewCell.swift
+//  WordPressBlogApp
+//
+//  Created by Steven Taglohner on 31/08/2017.
+//  Copyright © 2017 Steven Taglohner. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WordPressBlogApp/WordPressBlogApp/SupportFile.swift
@@ -0,0 +1,14 @@
+//
+//  SupportFile.swift
+//  WordPressBlogApp
+//
+//  Created by Steven Taglohner on 31/08/2017.
+//  Copyright © 2017 Steven Taglohner. All rights reserved.
+//
+
+import Foundation
+
+extension RequestWordPressData {
+    
+    
+}
